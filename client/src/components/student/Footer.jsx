35cbad0cabdc4216d0dae964184b5b7337726344
kamruzzamanBappy <kamruzzamanbappy13@gmail.com>
2025-05-10 22:39:26 +0600

    d6

--- client/src/components/student/Footer.jsx
+++ client/src/components/student/Footer.jsx
@@ -6,12 +6,37 @@ const Footer = () => {
   <div className="flex flex-col md:flex-row items-star px-8 md:px-0 justify-center gap-10 md:gap-32 py-10 border-b border-white/30">
     <div className="flex flex-col md:items-start items-center w-full">
       <img src={assets.logo_dark} alt="logo" />
-  <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Qui praesentium eum sapiente, dolorum alias distinctio ea, placeat nobis delectus consequatur eveniet ullam nesciunt reprehenderit commodi fugit! Facere nisi excepturi quidem.</p>
+  <p className="mt-6 text-center md:text-left text-sm text-white/80">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Qui praesentium eum sapiente, dolorum alias distinctio ea, placeat nobis delectus consequatur eveniet ullam nesciunt reprehenderit commodi fugit! Facere nisi excepturi quidem.</p>
     </div>
-    <div></div>
-    <div></div>
+    <div className="flex flex-col md:items-start items-center w-full">
+      <h2 className="font-semibold text-white mb-5">Company</h2>
+      <ul className="flex md:flex-col w-full justify-between text-sm text-white/80 md:space-y-2">
+        <li><a href="#">Home</a></li>
+        <li><a href="#">About us</a></li>
+        <li><a href="#">Contact us</a></li>
+        <li><a href="#">Privacy policy</a></li>
+   
+      </ul>
+    </div>
+    <div className="hidden md:flex flex-col items-start w-full" >
+
+<h2 className="font-semibold text-white mb-5">
+  Subscribe our newsletter
+</h2>
+<p className="text-sm text-white/80">
+The latest news,articles and resources,send to your inbox weekly
+ </p>
+ <div className="flex items-center gap-2 pt-4">
+
+<input type="email" placeholder="Enter your email" className="border border-gray-500/30 bg-gray-800  text-gray-500 placeholder-gray-500 outline-none w-64 h-9 rounded px-2 text-sm " />
+
+<button className="bg-blue-600 w-24 h-9 text-white rounded">Subscribe</button>
+
+ </div>
+ 
+    </div>
   </div>
-  <p>Copyright 2025 @ Educare. All right reserved. </p>
+  <p className="py-4 text-center text-xs md:text-sm text-white/60">Copyright 2025 @ Educare. All right reserved. </p>
  </footer>
   );
 };
--- client/src/pages/student/CourseDetails.jsx
+++ client/src/pages/student/CourseDetails.jsx
@@ -1,9 +1,60 @@
+import { useContext, useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { assets } from "../../assets/assets";
+import Loading from "../../components/student/Loading";
+import { AppContext } from "../../context/AppContext";
+
 const CourseDetails = () => {
-  return (
-    <div>
-      <h1>CourseDetails</h1>
-    </div>
-  );
+
+  const {id} = useParams()
+  const [courseData, setCourseData] = useState(null)
+
+  const {allCourses,calculateRating} = useContext(AppContext)
+
+  const fetchCourseData = async () =>{
+    const findCourse = allCourses.find(course => course._id === id )
+   setCourseData(findCourse)
+  }
+
+  useEffect(() =>{
+    fetchCourseData()
+  },[])
+
+  return courseData ? (
+    <>
+    <div className="flex md:flex-row flex-col-reverse gap-10 relative items-start justify-between md:px-36 px-8 md:pt-30 pt-20 text-left">
+
+<div className="absolute top-0 left-0 w-full h-[600px] bg-gradient-to-b from-cyan-100/70">
+</div>
+
+{/*left column */}
+<div className="max-w-xl z-10 text-gray-500">
+ <h1 className="md:text-xl text-[44px] font-semibold text-gray-800">{courseData.courseTitle}</h1>
+<p className="pt-4 md:text-base text-sm" dangerouslySetInnerHTML={{__html:courseData.courseDescription.slice(0,200)}}></p>
+
+{/*review and ratings */}
+   <div className='flex items-center space-x-2 pt-3 pb-1 text-sm '>
+        <p>{calculateRating(courseData)}</p>
+        <div className='flex'>
+          {[...Array(5)].map((_,i)=>(  <img key={i} src={i< Math.floor(calculateRating(courseData)) ? assets.star:assets.star_blank} alt='' className='w-3.5 h-3.5'  />)
+        )}
+        </div>
+
+        <p className='text-blue-600'>{courseData.courseRatings.length} {courseData.courseRatings.length > 1 ? 'Ratings':'Rating'}</p>
+
+<p>{courseData.enrolledStudents.length}{courseData.enrolledStudents.length > 1 ? 'students':'student'}</p>
+
+      </div>
+<p className="text-sm">Course by <span className="text-blue-600 underline">Educare</span></p>
+</div>
+
+{/* right column*/}
+<div>
+ 
+</div>
+
+    </div> </>
+  ) : <Loading />
 };
 
 export default CourseDetails;
