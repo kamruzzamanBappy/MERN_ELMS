1ff7662a7056a5b4433de8af6312673d70fa4398
kamruzzamanBappy <kamruzzamanbappy13@gmail.com>
2025-05-12 12:01:22 +0600

    d7

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,3 +1,5 @@
+ 
+import humanizeDuration from 'humanize-duration';
 import { createContext, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { dummyCourses } from "../assets/assets";
@@ -14,6 +16,11 @@ export const AppContextProvider = (props) => {
  const [isEducator,setIsEducator]=useState(true)
 
 
+ //Fetch All Courses
+  const fetchAllCourses = async ()=>{
+  setAllCourses(dummyCourses)
+ }
+
  //Function to cal the average rating of course
  const calculateRating = (course)=>{
   if(course.courseRatings.length === 0){
@@ -25,11 +32,41 @@ export const AppContextProvider = (props) => {
   })
   return totalRating / course.courseRatings.length
  }
- 
- //Fetch All Courses
- const fetchAllCourses = async ()=>{
-  setAllCourses(dummyCourses)
+
+//Function to Calculate Course Chapter Taime
+ const calculateChapterTime = (chapter) =>{
+
+  let time = 0
+  chapter.chapterContent.map((lecture) =>time +=lecture.lectureDuration)
+  return humanizeDuration(time*60*1000,{units:["h","m"]})
  }
+
+ // Function to calculate course duration
+
+ const calculateCourseDuration = (course)=>{
+  let time = 0
+
+  course.courseContent.map((chapter) => chapter.chapterContent.map((lecture) => time +=lecture.lectureDuration))
+
+  return humanizeDuration(time * 60 * 1000,{units:["h","m"]})
+ }
+
+
+ // Function calculate to No of lecture in this code
+
+ const calculateNoOfLectures = (course) =>{
+  let totalLectures = 0;
+  course.courseContent.forEach(chapter =>{
+    if(Array.isArray(chapter.chapterContent)){
+      totalLectures += chapter.chapterContent.length
+    }
+  });
+  return totalLectures
+ }
+
+ 
+
+
 
  useEffect(()=>{
   fetchAllCourses()
@@ -37,7 +74,7 @@ export const AppContextProvider = (props) => {
  
  const value = {
     currency,allCourses,navigate,calculateRating,
-  isEducator,setIsEducator
+  isEducator,setIsEducator,calculateNoOfLectures,calculateCourseDuration, calculateChapterTime,
   
   };
 
