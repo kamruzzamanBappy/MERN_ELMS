b70e7ae3b82ba7a6f66bb23dbc4badb0360427ef
kamruzzamanBappy <kamruzzamanbappy13@gmail.com>
2025-05-18 11:16:33 +0600

    first commit

--- server/controllers/webhooks.js
+++ server/controllers/webhooks.js
@@ -16,9 +16,8 @@ export const clerkWebhooks = async (req,res)=>{
  })
 
  const {data,type} = req.body
-    } catch (error) {
-      
-        switch (type) {
+
+  switch (type) {
             case 'user.created': {
                 const userData = {
                     _id: data.id,
@@ -26,15 +25,34 @@ export const clerkWebhooks = async (req,res)=>{
                     name: data.first_name + " " + data.last_name,
                     imageUrl:data.image_url,
                 }
-
+//save data by await
                 await User.create(userData)
                 res.json({})
                 break;
             }
 
+case 'user.updated': {
+
+         const userData = {
+                    email: data.email_address[0].email_address,
+                    name: data.first_name + " " + data.last_name,
+                    imageUrl:data.image_url,
+                }
+await User.findByIdAndUpdate(data.id,userData)
+res.json({})
+break;
+}
+
+case 'user.deleted' :{
+    await User.findByIdAndDelete(data.id)
+    res.json({})
+    break;
+}
             default:break;
         }
 
-
+    } catch (error) {
+      res.json({success:false, message:error.message})
+      
     }
 }
--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import cors from 'cors'
 import 'dotenv/config'
 import express from 'express'
 import connectDB from './configs/mongodb.js'
+import { clerkWebhooks } from './controllers/webhooks.js'
 
 
 //Initialize Express
@@ -16,7 +17,7 @@ app.use(cors())
 
 //Route 
 app.get('/',(req,res)=> res.send("App Working"))
-
+app.post('/clerk', express.json(),clerkWebhooks)
 //Port
 const PORT = process.env.PORT || 5000
 
